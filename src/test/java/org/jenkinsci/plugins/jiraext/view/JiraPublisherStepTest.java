b6ab824431104712ff20aa61fc993ec33988af9c
Dan Alvizu <dalvizu@pingidentity.com>
2016-12-12 10:05:42 -0700

    fix copyright header

--- src/test/java/org/jenkinsci/plugins/jiraext/view/JiraPublisherStepTest.java
+++ src/test/java/org/jenkinsci/plugins/jiraext/view/JiraPublisherStepTest.java
@@ -1,17 +1,20 @@
 /***************************************************************************
- * Copyright (C) 2014 Ping Identity Corporation
- * All rights reserved.
- * <p/>
- * The contents of this file are the property of Ping Identity Corporation.
- * You may not copy or use this file, in either source code or executable
- * form, except in compliance with terms set by Ping Identity Corporation.
- * For further information please contact:
- * <p/>
- * Ping Identity Corporation
- * 1001 17th Street Suite 100
- * Denver, CO 80202
- * 303.468.2900
- * http://www.pingidentity.com
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  **************************************************************************/
 package org.jenkinsci.plugins.jiraext.view;
 
